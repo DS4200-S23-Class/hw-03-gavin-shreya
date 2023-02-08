1b02ab056d86ef76ea27cc021704b9f7a72d230b
Gavin Mooallem <gmooallem@gmail.com>
2023-02-07 19:17:33 -0500

    Add third visualization

---
+++ main.js
@@ -0,0 +1,8 @@
+//function to change the background to red when the button is clicked
+function changeBackground() {
+	if(document.body.style.backgroundColor == "red") {
+		document.body.style.backgroundColor = "white";
+	} else {
+		document.body.style.backgroundColor = "red";
+	}
+}
